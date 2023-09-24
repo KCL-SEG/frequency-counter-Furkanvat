96b92d84c8292c45a5d15f74c58485f9e3270c23
Furkanvat <117448902+Furkanvat@users.noreply.github.com>
2023-09-24 18:20:42 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in items:
+        if i in frequencies:
+            frequencies[i] =  frequencies[i] + 1
+        else:
+            frequencies[i] = 1
     return frequencies
